fac21146fa2032777e03c59b3450745e9ad9135e
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-11-12 11:09:57 +1300

    rework to hide content instead of delete

--- content-script.js
+++ content-script.js
@@ -1,6 +1,6 @@
 let element = null
 let previousElement = null
-let previousParentNode = null
+let previousDisplayStyle = null
 
 /**
  * Every time the context menu is opened, (using right click), the html element the mouse was on is saved
@@ -12,15 +12,18 @@ document.addEventListener('contextmenu', (e) => {
 
 /**
  * Creates listener thats recieves any messages from the background script
- * If the action is remove, try to delete the element previously selected
+ * If the action is remove, save target element display style and save target element, set display to none to hide the content
+ * 
+ * If the action is undo, set elements display to what it previously was
  */
 
 browser.runtime.onMessage.addListener(async (request) => {
   if(request.action === 'remove') {
     try {
       previousElement = element
-      previousParentNode = element.parentNode
-      element.parentNode.removeChild(element)
+      previousDisplayStyle = element.style.display
+      element.style.display = "none"
+ 
       element = null
     } 
     catch (error) {
@@ -29,9 +32,9 @@ browser.runtime.onMessage.addListener(async (request) => {
   }
   if(request.action === 'undo') {
     try {
-      previousParentNode.parentNode.appendChild(previousElement)
+      previousElement.style.display = previousDisplayStyle
       previousElement = null
-      previousParentNode = null
+      previousDisplayStyle = null
     }
     catch (error) {
       console.log(error)
