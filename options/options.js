038d5142688e77c2ff66a4267d52da1b83dc6486
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-11-17 14:52:14 +1300

    add options page

---
+++ options/options.js
@@ -0,0 +1,50 @@
+const checkbox = document.getElementById('undoMenu')
+
+/**
+ * Save and retrieve settings
+ */
+
+ const saveOptions = (boolean) => {
+  browser.storage.sync.set({
+    checkStatus: boolean
+  })
+}
+
+const restoreOptions = () => {
+  browser.storage.sync.get('checkStatus')
+  .then(data => {
+    checkbox.checked = data.checkStatus
+  })
+}
+
+/**
+ * Sends message to background script, will add or remove undo context menu:
+ *    If checkbox is checked the menu is shown,
+ *    so the removeMenu message is false. The opposite.
+ */
+
+const removeUndoContextMenu = (checkBoxIsChecked) => {
+  browser.runtime.sendMessage({removeMenu: !checkBoxIsChecked})
+}
+
+console.log('options')
+
+/**
+ * Listens for clicks on checkbox:
+ *    Saves the new checkbox status
+ *    Sends message to background script, to add or remove context menu
+ */
+
+ checkbox.addEventListener('click', (e) => {
+   console.log('checkbox')
+  checkbox.checked = e.target.checked
+  saveOptions(e.target.checked)
+  removeUndoContextMenu(e.target.checked)
+})
+
+/**
+ * Listens for when the popup is opened:
+ *    gets saved options
+ */
+
+document.addEventListener("DOMContentLoaded", restoreOptions)
